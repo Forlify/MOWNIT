207af57ece3d437589de67a331e0e90dd224de5f
Forlify <forlify@gmail.com>
2020-03-10 14:39:36 +0100

    lab 2

--- lab-2/task-1.py
+++ lab-2/task-1.py
@@ -25,15 +25,15 @@ def approximation(n, count=500):
     return results
 
 
-plt.plot(approximation(5))
-# plt.plot(approximation(10))
-# plt.plot(approximation(15))
+plt.plot(np.linspace(-5, 5, 500), approximation(5))
+plt.plot(np.linspace(-5, 5, 500), approximation(10))
+plt.plot(np.linspace(-5, 5, 500), approximation(15))
 
 real_function = []
 for x in np.linspace(-5, 5, 500):
     real_function.append(function(x))
 
-plt.plot(real_function)
+plt.plot(np.linspace(-5, 5, 500), real_function)
 plt.show()
 
 
@@ -46,27 +46,43 @@ def calculate_difference(n):
 
     results = list(map(lambda x, y: x - y, results, list(map(function, x))))
 
-    plt.plot(results)
-    plt.show()
+    return results
 
 
-calculate_difference(5)
-calculate_difference(10)
-calculate_difference(15)
+plt.plot(np.linspace(-5, 5, 30), calculate_difference(5))
+plt.plot(np.linspace(-5, 5, 30), calculate_difference(10))
+plt.plot(np.linspace(-5, 5, 30), calculate_difference(15))
+plt.show()
+
 
 #######################################################################################
+
+def count_chebyshev_x(n, k, a, b):
+    return ((1.0 / 2.0) * (a + b)) + ((1.0 / 2.0) * (b - a) * np.cos(((2.0 * k - 1.0) * np.pi) / (2.0 * n)))
 
 
 def chebyshev_approximation(n, count=500):
     results = []
-    x = np.linspace(-5, 5, n + 1)
-    vander_x = np.vander(x, n + 1, True)
+    x = []
+    for i in range(1, n+1):
+        x.append(count_chebyshev_x(n, i, -5, 5))
+
+    print(x)
+    vander_x = np.vander(x, n , True)
     coefficients = np.linalg.solve(vander_x, list(map(function, x)))
 
-    for x in np.linspace(-5, 5, count):
+    for x in np.linspace(-5, 5, 500):
         result = 0
         for index, coefficient in enumerate(coefficients):
             result += coefficient * (x ** index)
         results.append(result)
 
     return results
+
+
+# plt.plot(chebyshev_approximation(5))
+# plt.plot(chebyshev_approximation(10))
+plt.plot(np.linspace(-5, 5, 500), chebyshev_approximation(20))
+plt.plot(np.linspace(-5, 5, 500), real_function)
+
+plt.show()
