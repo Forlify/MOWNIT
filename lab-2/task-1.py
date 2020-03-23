aed55c11b7b6b83f26de28d2a5f0370e026f5fe8
Forlify <forlify@gmail.com>
2020-03-23 19:01:38 +0100

    lab3 done

--- lab-2/task-1.py
+++ lab-2/task-1.py
@@ -61,13 +61,12 @@ def count_chebyshev_x(n, k, a, b):
     return ((1.0 / 2.0) * (a + b)) + ((1.0 / 2.0) * (b - a) * np.cos(((2.0 * k - 1.0) * np.pi) / (2.0 * n)))
 
 
-def chebyshev_approximation(n, count=500):
+def chebyshev_approximation(n):
     results = []
     x = []
     for i in range(1, n+1):
         x.append(count_chebyshev_x(n, i, -5, 5))
 
-    print(x)
     vander_x = np.vander(x, n , True)
     coefficients = np.linalg.solve(vander_x, list(map(function, x)))
 
@@ -82,5 +81,39 @@ def chebyshev_approximation(n, count=500):
 
 plt.plot(np.linspace(-5, 5, 500), chebyshev_approximation(15))
 plt.plot(np.linspace(-5, 5, 500), real_function)
+plt.show()
 
+#######################################################################################
+
+
+
+a = 10
+b = 5
+
+
+def function_x(t):
+    return a * np.cos(t)
+
+
+def function_y(t):
+    return b * np.sin(t)
+
+
+def interpolate_elipse(n):
+    points = np.linspace(0, 2 * np.pi, n)
+
+    spline_x = interpolate.interp1d(points, list(map(function_x, points)), kind='cubic')
+    spline_y = interpolate.interp1d(points, list(map(function_y, points)), kind='cubic')
+
+    xs = []
+    ys = []
+
+    for t in np.linspace(0, 2 * np.pi, 30):
+        xs.append(spline_x(t))
+        ys.append(spline_y(t))
+
+    plt.plot(xs, ys)
+
+
+interpolate_elipse(10)
 plt.show()
